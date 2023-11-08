2e0eba2ee96069c673ee7a20cb24e62fca112aea
FedeBramb <146201363+FedeBramb@users.noreply.github.com>
2023-11-08 16:09:48 +0100

    upgrade

--- src/index.js
+++ src/index.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Card from './Card';
 import reportWebVitals from './reportWebVitals';
+import 'tachyons';
+import { robots } from '/.robots'; /*If you only need to export a single value from a module, or if the module represents a main feature of your application, use export default . If you need to export multiple values from a module, or if you want to organize your code into smaller, reusable components, use export with named exports.*/
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+
+
+ReactDOM.createRoot(
+    document.getElementById('root')
+).render(
+    <React.StrictMode>
+        <div>
+            <Card id={robots[0].id} name={robots[0].name email={robots[0].email}/>
+            <Card id={robots[0].id} name={robots[0].name email={robots[0].email}/>
+            <Card id={robots[0].id} name={robots[0].name email={robots[0].email}/>
+        </div>
+    </React.StrictMode>
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
