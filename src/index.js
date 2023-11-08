361de23e8a54a956c199e8fabe7963ca173f192f
FedeBramb <146201363+FedeBramb@users.noreply.github.com>
2023-11-08 17:05:56 +0100

    upgrade

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import Card from './Card';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
-import { robots } from '/.robots'; /*If you only need to export a single value from a module, or if the module represents a main feature of your application, use export default . If you need to export multiple values from a module, or if you want to organize your code into smaller, reusable components, use export with named exports.*/
+import { robots } from './robots'; /*If you only need to export a single value from a module, or if the module represents a main feature of your application, use export default . If you need to export multiple values from a module, or if you want to organize your code into smaller, reusable components, use export with named exports.*/
 
 
 
@@ -13,9 +13,9 @@ ReactDOM.createRoot(
 ).render(
     <React.StrictMode>
         <div>
-            <Card id={robots[0].id} name={robots[0].name email={robots[0].email}/>
-            <Card id={robots[0].id} name={robots[0].name email={robots[0].email}/>
-            <Card id={robots[0].id} name={robots[0].name email={robots[0].email}/>
+            <Card id={robots[0].id} name={robots[0].name} email={robots[0].email}/>
+            <Card id={robots[1].id} name={robots[1].name} email={robots[1].email}/>
+            <Card id={robots[2].id} name={robots[2].name} email={robots[2].email}/>
         </div>
     </React.StrictMode>
 );
