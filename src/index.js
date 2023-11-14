3bf923e1ee0bf2326ee5fe9eb66dffc7d8ad1b10
FedeBramb <146201363+FedeBramb@users.noreply.github.com>
2023-11-14 19:25:22 +0100

    update

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './containers/App';
 import reportWebVitals from './reportWebVitals';
@@ -8,13 +8,8 @@ import 'tachyons';
 
 
 
-ReactDOM.createRoot(
-    document.getElementById('root')
-).render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
-);
+const root = createRoot(document.getElementById('root'));
+root.render(<App />);
 
 
 // If you want to start measuring performance in your app, pass a function
