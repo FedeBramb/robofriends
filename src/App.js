aee8e9773b9f0f9a8a0d5fd31ff6f27520656609
FedeBramb <146201363+FedeBramb@users.noreply.github.com>
2023-11-14 17:57:14 +0100

    update

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'; /*per utilizzare la classe App*/
 import CardList from './CardList';
 import SearchBox from './SearchBox';
 import { robots } from './robots';
+import Scroll from './Scroll';
 import './App.css';
 
 /*utilizziamo lo state, è un object*/
@@ -17,7 +18,7 @@ class App extends Component {
     componentDidMount() {
         fetch('https://jsonplaceholder.typicode.com/users')
             .then(response=> response.json())
-            .then(users =>this.setState({ robots: users})
+            .then(users => this.setState({ robots: users}))
     }
 
     onSearchChange = (event) => { 
@@ -28,36 +29,19 @@ class App extends Component {
         const filteredRobots = this.state.robots.filter(robots =>{
             return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
         })
-        if (this.state.robots.lenght === 0) {
+        if (this.state.robots.length === 0) {
             return <h1>Loading...</h1>
         } else {
-
-        
             return (
                 <div className='tc'>
                     <h1 className='f1'>RoboFriends</h1>
                     <SearchBox searchChange={this.onSearchChange}/>
-                    <CardList robots={filteredRobots}/>
+                    <Scroll>    
+                        <CardList robots={filteredRobots}/>
+                    </Scroll>
                 </div>
             );
+        }
     }
 }
-
-export default App;
-
-/* La ragione principale per cui App è una classe 
-invece di una funzione normale è che stai utilizzando 
-la classe Component di React per definire un componente 
-di classe. I componenti di classe in React sono una forma 
-di classi ES6 che estendono la classe base React.Component.
-Questo ti consente di sfruttare le funzionalità offerte 
-da React, come lo state, i cicli di vita del componente, 
-e così via.
-Abbiamo App component che due stati che devono comunicare fra loro,
-robots e searchfield, passiamo a onSearch che ogni volta che avviene
-un cambiamento comunica al constructor cosa abbiamo digitato nel
-searchfield, che modifica gli states,
-ora abbiamo i dati che comunichiamo a CardList che filtriamo nei robots
-ciò che abbiamo nel searchfield */
-/*Ogni volta che creiamo un metodo con una classe bisogna utulizzare arrow function 
-se vogliamo cambiare state, this.setState({ searchfield: event.target.value })*/
+export default App; 
