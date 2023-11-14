fd1fa23ca71e2b8eda93cf46585691cb1df7b180
FedeBramb <146201363+FedeBramb@users.noreply.github.com>
2023-11-14 18:24:47 +0100

    update

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -1,13 +1,11 @@
 import React from 'react';
 
-// css: overflow-y jsx: overflowY
-// 'scroll' css way scrollable
 const Scroll = (props) => {
     return (
-        <div style={{overflowY: 'scroll', border: '1px solid black', height: '500px'}}>
+        <div style={{overflow: 'scroll', border: '1px solid black', height: '500px'}}>
             {props.children}
         </div>
-    )
+    );
 };
 
 export default Scroll;
